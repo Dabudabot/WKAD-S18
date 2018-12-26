be7f40484f62581c567d6f08fd9ab28f616a72fa
Daulet Tumbayev <Daulet.Tumbayev@acronis.com>
2018-12-26 16:42:32 +0300

    Initial commit

--- Moufiltr/kbdhelper.c
+++ Moufiltr/kbdhelper.c
@@ -1,73 +1,85 @@
 /*++
 	Innopolis University 2018
 	Module Name:
-		moufiltr.c
+		kbdhelper.c
 	Abstract:
 		This module contains implementations
 	Environment:
 		Kernel mode only
 --*/
 
-#include "moufiltr.h"
+#include "kbdhelper.h"
 
-NTSTATUS DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING registryPath)
+NTSTATUS
+DriverEntry(
+PDRIVER_OBJECT driverObject,
+PUNICODE_STRING registryPath
+)
 {
 	UNREFERENCED_PARAMETER(registryPath);
 
 	driverObject->DriverUnload = DriverUnload;
 
-	for (int i = 1; i <= IRP_MJ_MAXIMUM_FUNCTION; i++) 
+	for (int i = 1; i <= IRP_MJ_MAXIMUM_FUNCTION; i++)
 	{
 		driverObject->MajorFunction[i] = DispatchPass;
 	}
 
 	driverObject->MajorFunction[IRP_MJ_READ] = DispatchRead;
 
-	const NTSTATUS status = MyAttachDevice(driverObject);
-	if (!NT_SUCCESS(status)) {
-		KdPrint(("Attaching Mouse_Filter_Driver is failed \r\n"));
-	}
-	else
-	{
-		KdPrint(("Attacning Mouse_Filter_Driver is succeeds \r\n"));
-	}
+	NTSTATUS status = MyAttachDevice(driverObject);
+
+  if (NT_SUCCESS(status))
+  {
+    DbgPrint("Attach\n");
+  }
+
 	return status;
 }
 
-VOID DriverUnload(PDRIVER_OBJECT driverObject)
+VOID
+DriverUnload(
+PDRIVER_OBJECT driverObject
+)
 {
 	LARGE_INTEGER interval = { 0 };
 	PDEVICE_OBJECT deviceObject = driverObject->DeviceObject;
 	interval.QuadPart = -10 * 1000 * 1000;
 
-	while (deviceObject) 
+	while (deviceObject)
 	{
 		IoDetachDevice(((pdevice_extension)deviceObject->DeviceExtension)->m_lowerDevice);
 		deviceObject = deviceObject->NextDevice;
 	}
 
-	while (pendingkey) 
+	while (pendingkey)
 	{
 		KeDelayExecutionThread(KernelMode, FALSE, &interval);
 	}
 
 	deviceObject = driverObject->DeviceObject;
-	while (deviceObject) 
+	while (deviceObject)
 	{
 		IoDeleteDevice(deviceObject);
 		deviceObject = deviceObject->NextDevice;
 	}
-
-	KdPrint(("Unload MouseFilterDriver \r\n"));
 }
 
-NTSTATUS DispatchPass(PDEVICE_OBJECT deviceObject, PIRP irp)
+NTSTATUS
+DispatchPass(
+PDEVICE_OBJECT deviceObject,
+PIRP irp
+)
 {
 	IoCopyCurrentIrpStackLocationToNext(irp);
 	return IoCallDriver(((pdevice_extension)deviceObject->DeviceExtension)->m_lowerDevice, irp);
 }
 
-NTSTATUS DispatchRead(PDEVICE_OBJECT deviceObject, PIRP irp)
+NTSTATUS
+DispatchRead(
+PDEVICE_OBJECT deviceObject,
+PIRP irp
+)
 {
 	IoCopyCurrentIrpStackLocationToNext(irp);
 	IoSetCompletionRoutine(irp, ReadComplete, NULL, TRUE, TRUE, TRUE);
@@ -77,15 +89,22 @@ NTSTATUS DispatchRead(PDEVICE_OBJECT deviceObject, PIRP irp)
 	return IoCallDriver(((pdevice_extension)deviceObject->DeviceExtension)->m_lowerDevice, irp);
 }
 
-NTSTATUS ReadComplete(PDEVICE_OBJECT deviceObject, PIRP irp, PVOID context)
+//TODO: logic goes here
+
+NTSTATUS
+ReadComplete(
+PDEVICE_OBJECT deviceObject,
+PIRP irp,
+PVOID context
+)
 {
 	UNREFERENCED_PARAMETER(deviceObject);
 	UNREFERENCED_PARAMETER(context);
 	pmouse_input_data keys = (pmouse_input_data)irp->AssociatedIrp.SystemBuffer;
 
-	if (irp->IoStatus.Status == STATUS_SUCCESS) 
+	if (irp->IoStatus.Status == STATUS_SUCCESS)
 	{
-		for (int i = 0; i < irp->IoStatus.Information / sizeof(mouse_input_data); i++) 
+		for (int i = 0; i < irp->IoStatus.Information / sizeof(mouse_input_data); i++)
 		{
 			const USHORT flag = keys->m_buttons.button_data.m_buttonFlags;
 
@@ -113,7 +132,7 @@ NTSTATUS ReadComplete(PDEVICE_OBJECT deviceObject, PIRP irp, PVOID context)
 					currentPoint = 0;
 				}
 			}
-			
+
 			if (isInverse)
 			{
 				keys->m_lastY = 65535 - keys->m_lastY;
@@ -121,7 +140,7 @@ NTSTATUS ReadComplete(PDEVICE_OBJECT deviceObject, PIRP irp, PVOID context)
 		}
 	}
 
-	if (irp->PendingReturned) 
+	if (irp->PendingReturned)
 	{
 		IoMarkIrpPending(irp);
 	}
@@ -130,7 +149,12 @@ NTSTATUS ReadComplete(PDEVICE_OBJECT deviceObject, PIRP irp, PVOID context)
 	return irp->IoStatus.Status;
 }
 
-NTSTATUS MyAttachDevice(PDRIVER_OBJECT driverObject)
+//TODO: change to kbd
+
+NTSTATUS
+MyAttachDevice(
+PDRIVER_OBJECT driverObject
+)
 {
 	UNICODE_STRING mouseClassName = RTL_CONSTANT_STRING(L"\\Driver\\Mouclass");
 	PDRIVER_OBJECT targetDriverObject = NULL;
@@ -138,9 +162,9 @@ NTSTATUS MyAttachDevice(PDRIVER_OBJECT driverObject)
 	PDEVICE_OBJECT myDeviceObject = NULL;
 
 	NTSTATUS status = ObReferenceObjectByName(&mouseClassName,
-		OBJ_CASE_INSENSITIVE, NULL, 0, *IoDriverObjectType, 
+		OBJ_CASE_INSENSITIVE, NULL, 0, *IoDriverObjectType,
 		KernelMode, NULL, (PVOID*)&targetDriverObject);
-	if (!NT_SUCCESS(status)) 
+	if (!NT_SUCCESS(status))
 	{
 		KdPrint(("ObReference  is failed \r\n"));
 		return status;
@@ -150,20 +174,20 @@ NTSTATUS MyAttachDevice(PDRIVER_OBJECT driverObject)
 
 	currentDeviceObject = targetDriverObject->DeviceObject;
 
-	while (currentDeviceObject != NULL) 
+	while (currentDeviceObject != NULL)
 	{
-		status = IoCreateDevice(driverObject, sizeof(device_extension), NULL, FILE_DEVICE_MOUSE, 
+		status = IoCreateDevice(driverObject, sizeof(device_extension), NULL, FILE_DEVICE_MOUSE,
 			0, FALSE, &myDeviceObject);
-		if (!NT_SUCCESS(status)) 
+		if (!NT_SUCCESS(status))
 		{
 			return status;
 		}
 
 		RtlZeroMemory(myDeviceObject->DeviceExtension, sizeof(device_extension));
-		status = IoAttachDeviceToDeviceStackSafe(myDeviceObject, currentDeviceObject, 
+		status = IoAttachDeviceToDeviceStackSafe(myDeviceObject, currentDeviceObject,
 			&((pdevice_extension)myDeviceObject->DeviceExtension)->m_lowerDevice);
 
-		if (!NT_SUCCESS(status)) 
+		if (!NT_SUCCESS(status))
 		{
 			return status;
 		}
--- Moufiltr/kbdhelper.h
+++ Moufiltr/kbdhelper.h
@@ -1,7 +1,7 @@
 /*++
 	Innopolis University 2018
 	Module Name:
-		moufiltr.h
+		kbdhelper.h
 	Abstract:
 		This module contains the common private declarations for the mouse packet filter
 		Needed to hook mouse input and modify
@@ -38,7 +38,7 @@ NTSTATUS ObReferenceObjectByName(
 /**
 * \brief device extension to passthrought
 */
-typedef struct 
+typedef struct
 {
 	PDEVICE_OBJECT m_lowerDevice;
 } device_extension, *pdevice_extension;
@@ -46,14 +46,14 @@ typedef struct
 /**
 * \brief mouse data stored here
 */
-typedef struct 
+typedef struct
 {
 	USHORT m_unitId;
 	USHORT m_flags;
-	union 
+	union
 	{
 		ULONG  buttons;
-		struct 
+		struct
 		{
 			USHORT m_buttonFlags;
 			USHORT m_buttonData;
@@ -99,16 +99,16 @@ VOID DriverUnload(PDRIVER_OBJECT driverObject);
 /**
  * \brief default pass to any callbacks
  * \param deviceObject driver object itself
- * \param irp 
+ * \param irp
  * \return result of function
  */
 NTSTATUS DispatchPass(PDEVICE_OBJECT deviceObject, PIRP irp);
 
 /**
- * \brief 
- * \param deviceObject 
- * \param irp 
- * \param context 
+ * \brief
+ * \param deviceObject
+ * \param irp
+ * \param context
  * \return result of function
  */
 NTSTATUS ReadComplete(PDEVICE_OBJECT deviceObject, PIRP irp, PVOID context);
@@ -116,7 +116,7 @@ NTSTATUS ReadComplete(PDEVICE_OBJECT deviceObject, PIRP irp, PVOID context);
 /**
  * \brief spetial pass for IRP_MJ_READ
  * \param deviceObject driver object itself
- * \param irp 
+ * \param irp
  * \return result of function
  */
 NTSTATUS DispatchRead(PDEVICE_OBJECT deviceObject, PIRP irp);
